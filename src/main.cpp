b8a6cf533ac5aa790633a369024529fe803a82f8
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2023-01-01 14:56:20 +0100

    Sync with NTP time and display it

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 #include <Adafruit_Neopixel.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include "ESP8266TimerInterrupt.h"
 
 #include <secrets.h>
 
@@ -21,6 +24,9 @@
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(6, PIXEL_PIN, NEO_GRB + NEO_KHZ400);
 WiFiClient client;
+WiFiUDP ntp_udp;
+NTPClient time_client(ntp_udp);
+ESP8266Timer ITimer;
 
 
 void strip_number(uint8_t v) {
@@ -89,6 +95,17 @@ void printWiFiStatus() {
   Serial.println(" dBm");
 }
 
+
+bool ntp_synced = false;
+unsigned long long int seconds_since_ntp_sync = 0;
+
+#define TIMER_INTERVAL_MS 1000
+void IRAM_ATTR TimerHandler()
+{
+  if (ntp_synced)
+    seconds_since_ntp_sync += 1;
+}
+
 void setup() 
 {
   Serial.begin(115200);
@@ -111,6 +128,13 @@ void setup()
 
   WiFi.begin(ssid, pass);
   Serial.println("connecting");
+
+  if (ITimer.attachInterruptInterval(TIMER_INTERVAL_MS * 1000, TimerHandler)) {
+    Serial.println(F("Starting  ITimer OK, millis() = "));
+  }
+  else {
+    Serial.println(F("Can't set ITimer correctly. Select another freq. or interval"));
+  }
 }
 
 void loop ()
@@ -122,28 +146,45 @@ void loop ()
     connection_status = WL_CONNECTED;
     Serial.println("WiFi Connected");
     printWiFiStatus();
+    time_client.begin();
+    time_client.update();
+    Serial.print("Current time: ");
+    Serial.println(time_client.getFormattedTime());
+    ntp_synced = true;
   }
-  for(int i = 0; i <= 9; i++)
-  {
-    Serial.print("Number: ");
-    Serial.println(i);
-    strip_number(i);
-    NixieDisplay(i);                // Do simple counting
-    NixieDisplay(i);                // Do simple counting
-    NixieDisplay(i);                // Do simple counting
-    NixieDisplay(i);                // Do simple counting
-    for (int j = 0; j < 100; j++) {
-      btn_value = digitalRead(BTN1_PIN);
-      if (btn1_state && btn_value != btn1_state) {
-        printf("BTN1 pressed\n");
+
+  if (ntp_synced) {
+    uint8_t h = time_client.getHours();
+    uint8_t m = time_client.getMinutes();
+    NixieDisplay(m % 10);
+    NixieDisplay(m / 10);
+    NixieDisplay(h % 10);
+    NixieDisplay(h / 10);
+    Serial.print("Updating time to ");
+    Serial.println(time_client.getFormattedTime());
+  } else {
+    for(int i = 0; i <= 9; i++)
+    {
+      Serial.print("Number: ");
+      Serial.println(i);
+      strip_number(i);
+      NixieDisplay(i);                // Do simple counting
+      NixieDisplay(i);                // Do simple counting
+      NixieDisplay(i);                // Do simple counting
+      NixieDisplay(i);                // Do simple counting
+      for (int j = 0; j < 1000; j++) {
+        btn_value = digitalRead(BTN1_PIN);
+        if (btn1_state && btn_value != btn1_state) {
+          printf("BTN1 pressed\n");
+        }
+        btn1_state = btn_value;
+        btn_value = digitalRead(BTN2_PIN);
+        if (btn2_state && btn_value != btn2_state) {
+          printf("BTN2 pressed\n");
+        }
+        btn2_state = btn_value;
+        delay(1);
       }
-      btn1_state = btn_value;
-      btn_value = digitalRead(BTN2_PIN);
-      if (btn2_state && btn_value != btn2_state) {
-        printf("BTN2 pressed\n");
-      }
-      btn2_state = btn_value;
-      delay(1);
     }
   }
 }
